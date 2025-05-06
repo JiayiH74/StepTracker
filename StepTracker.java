a6d523ce7550c65354ef75a5780da8d6af23403e
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 19:04:11 -0400

    Create StepTracker.java

---
+++ StepTracker.java
@@ -0,0 +1,17 @@
+public class StepTracker
+  {
+    private int days;
+    private int steps;
+    private int threshold;
+    private int activeDays;
+
+    public int activeDays()
+    {
+    }
+    public double averageSteps()
+    {
+    }
+    public void addDailySteps(int s)
+    {
+    }
+  }
